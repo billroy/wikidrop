a2019cf2ab27972b1e21bcbbff57c64903a5f333
Bill Roy <bill@bitlash.net>
2019-08-28 13:27:45 -0600

    wip sweep

--- upload.sh
+++ upload.sh
@@ -3,8 +3,7 @@ PORT="/dev/tty.usbserial-9152409F45"
 #HOME="/flash/"
 HOME="/"
 
-rshell -p ${PORT} cp main.py ${HOME}main.py
-rshell -p ${PORT} cp secrets.py ${HOME}secrets.py
-rshell -p ${PORT} cp ../MicroWebSrv/microWebSrv.py ${HOME}microWebSrv.py
-
-rshell -p ${PORT} cp www/index.html ${HOME}/www/index.html
+ampy -p ${PORT} put main.py
+ampy -p ${PORT} put secrets.py
+ampy -p ${PORT} put ../MicroWebSrv/microWebSrv.py
+ampy -p ${PORT} put /www
